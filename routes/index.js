b3e4e7b3641428afa18634a2b9c094e858b553de
connorwarme <connor.warme@gmail.com>
2023-01-27 19:24:42 -0700

    Add simple messages, in array

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  { 
+    text: "Yo, what's up?",
+    user: "Colin",
+    added: new Date()
+  },
+  {
+    text: "How're things?",
+    user: "Caleb",
+    added: new Date()
+  },
+  {
+    text: "How you doing?",
+    user: "Connor",
+    added: new Date()
+  }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
